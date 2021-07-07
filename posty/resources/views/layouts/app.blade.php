223468e4c84c3810646f7726e6204e5b313446a5
Sapae Safae <sapaebaamel@gmail.com>
2021-07-07 21:18:19 +0100

    authentification done

---
+++ posty/resources/views/layouts/app.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Posty</title>
+</head>
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between mb-6">
+        <ul class="flex items-center">
+            <li>
+                <a href="{{ route('home') }}"  class="p-3">Home</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Dashboard</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Post</a>
+            </li>
+        </ul>
+        <ul class="flex items-center">
+            @auth
+                <li>
+                    <a href="" class="p-3">Name</a>
+                </li>
+                <li>
+                    <form action="{{ route('logout')}}" class="p-3" method="POST">
+                        @csrf
+                        <button> LogOut </button>
+                    </form>
+                </li>
+            @endauth
+            @guest
+                <li>
+                    <a href="{{ route('login') }}" class="p-3">Login</a>
+                </li>
+                <li>
+                    <a href="{{ route('register') }}" class="p-3">Register</a>
+                </li>
+            @endguest
+        </ul>
+    </nav>
+    @yield('content')
+</body>
+</html>
